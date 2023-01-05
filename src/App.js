aea7c626391daf201ec836bacf0aacab7496723f
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-01-05 21:23:56 +0545

    firebase

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+    //BEM naming conviction
+    <div className="app">
+    <h1>Lets build the facebook clone</h1>
+      
+    </div>
+  );
+}
+
+export default App;
