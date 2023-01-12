7f29998b4a27773c221519a169f3ad89abfdb307
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-01-12 15:45:14 +0545

    MessageSender and input

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,6 +1,7 @@
 
 import { Avatar } from '@material-ui/core'
 import React from 'react'
+import { useState } from 'react';
 import "./MessageSender.css"
 import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
@@ -8,6 +9,7 @@ import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 function MessageSender() {
 
+    const [input, setInput] = useState('')
     const handleSubmit = (e)=>{
         e.preventDefault();
     }
@@ -15,7 +17,9 @@ function MessageSender() {
             <div className="messageSender_top">
                 <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXTTF25V8zJE5Xz9Wzk7q7ZFEVrV9DYO2hLQ&usqp=CAU'/>
                 <form action="">
-                    <input type="text" placeholder={'Whats on your mind'}/>
+                    <input value= {input } 
+                    onChange={(e)=>setInput(e.target.value)}
+                     className='messageSender_input' type="text" placeholder={`What's on your mind?`}/>
                     <input type="text" placeholder='Image URL (Optional)'/>
 
                     <button onClick={handleSubmit} type="submit">Hidden submit</button>
@@ -23,11 +27,18 @@ function MessageSender() {
             </div>
             <div className="messageSender_bottom">
                 <div className="messageSender_option">
+                <div>
                     <VideocamIcon style={{color:"red"}}/>
-                    <h3>Live Video</h3>
+                    <h3>Live Video</h3>    
+                </div>
+                <div>
                     <PhotoLibraryIcon style={{color:"green"}}/>
-                    <h3>Photo/Video</h3><InsertEmoticonIcon style={{color:"orange"}}/>
-                    <h3>Feelings/Activity</h3>
+                    <h3>Photo/Video</h3>       
+                </div>
+                <div>
+                    <InsertEmoticonIcon style={{color:"orange"}}/>
+                    <h3>Feelings/Activity</h3>   
+                </div>
                 </div>
             </div>
 
